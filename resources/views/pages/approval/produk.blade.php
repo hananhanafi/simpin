3bee81bb5b947191698494585ccdb25b4a88bf70
Rey-7 <renaldy.nst@gmail.com>
2023-01-06 02:01:58 +0700

    tambahan anggota

--- resources/views/pages/approval/produk.blade.php
+++ resources/views/pages/approval/produk.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('title')
-    Data Produk
+Data Produk
 @endsection
 
 @section('content')
@@ -60,101 +60,120 @@
 @endsection
 
 @section('css')
-    <!-- DataTables -->
-    <link href="{{ asset('assets') }}/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('assets') }}/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+<!-- DataTables -->
+<link href="{{ asset('assets') }}/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets') }}/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
 
-    <!-- Responsive datatable examples -->
-    <link href="{{ asset('assets') }}/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="{{asset('assets')}}/sweetalert/sweetalert.css">
+<!-- Responsive datatable examples -->
+<link href="{{ asset('assets') }}/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="{{asset('assets')}}/sweetalert/sweetalert.css">
 
-    <style>
-        td{
-            font-size:11px !important;
-        }
-        .parsley-errors-list li{
-            color : red !important;
-            font-style: italic;
-        }
-    </style>
+<style>
+    td {
+        font-size: 11px !important;
+    }
+
+    .parsley-errors-list li {
+        color: red !important;
+        font-style: italic;
+    }
+</style>
 @endsection
 
 @section('footscript')
-    <!-- Required datatable js -->
-    <script src="{{ asset('assets') }}/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="{{ asset('assets') }}/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <!-- Buttons examples -->
-    <script src="{{ asset('assets') }}/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="{{ asset('assets') }}/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
-    <script src="{{ asset('assets') }}/libs/jszip/jszip.min.js"></script>
-    <script src="{{ asset('assets') }}/libs/pdfmake/build/pdfmake.min.js"></script>
-    <script src="{{ asset('assets') }}/libs/pdfmake/build/vfs_fonts.js"></script>
-    <script src="{{ asset('assets') }}/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
-    <script src="{{ asset('assets') }}/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
-    <script src="{{ asset('assets') }}/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
+<!-- Required datatable js -->
+<script src="{{ asset('assets') }}/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="{{ asset('assets') }}/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+<!-- Buttons examples -->
+<script src="{{ asset('assets') }}/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="{{ asset('assets') }}/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+<script src="{{ asset('assets') }}/libs/jszip/jszip.min.js"></script>
+<script src="{{ asset('assets') }}/libs/pdfmake/build/pdfmake.min.js"></script>
+<script src="{{ asset('assets') }}/libs/pdfmake/build/vfs_fonts.js"></script>
+<script src="{{ asset('assets') }}/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="{{ asset('assets') }}/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="{{ asset('assets') }}/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
 
-    <!-- Responsive examples -->
-    <script src="{{ asset('assets') }}/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="{{ asset('assets') }}/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="{{ asset('assets') }}/sweetalert/sweetalert.min.js"></script>
-    <script type="text/javascript" src="{{ asset('js') }}/parsley.min.js"></script>
+<!-- Responsive examples -->
+<script src="{{ asset('assets') }}/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{ asset('assets') }}/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+<script type="text/javascript" charset="utf8" src="{{ asset('assets') }}/sweetalert/sweetalert.min.js"></script>
+<script type="text/javascript" src="{{ asset('js') }}/parsley.min.js"></script>
 
-    <script>
-        var dt = $('#datatable').DataTable({
-            "processing": true,
-            "serverSide": true,
-            "ajax":{
-                    "url": "{{ route('datatable.produk') }}",
-                    "dataType": "json",
-                    "data" : {
-                        'approval' : true
-                    }
-                },
-            "columns": [
-                { "data": 'DT_RowIndex' },
-                { "data": "kode" },
-                { "data": "nama_produk" },
-                { "data": "produk_tipe" },
-                { "data": "produk_jenis" },
-                { "data": "admin_fee" },
-                { "data": "status_produk" },
-                { "data": "aksi" },
-            ],
-            "columnDefs": [
-                { "className": 'text-center', "targets": [0,1,3,4,5,6,7] },
-            ]
-        });
-
-
-        function tolak(uuid){
-            var url = '{{ route("approval.produk.approve") }}';
-            $('#aproval-form').attr('action', url);
-            $('#id_produk').val(uuid)
-            swal({
-                title: "Apakah Anda Yakin !",
-                text: "Ingin Menolak Data Ini ?.",
-                type: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#802d34",
-                confirmButtonText: "Ya, Tolak",
-                cancelButtonText: "Cancel",
-                closeOnConfirm: false,
-                closeOnCancel: true
+<script>
+    var dt = $('#datatable').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "{{ route('datatable.produk') }}",
+            "dataType": "json",
+            "data": {
+                'approval': true
+            }
+        },
+        "columns": [{
+                "data": 'DT_RowIndex'
             },
-            function (isConfirm) {
+            {
+                "data": "kode"
+            },
+            {
+                "data": "nama_produk"
+            },
+            {
+                "data": "produk_tipe"
+            },
+            {
+                "data": "produk_jenis"
+            },
+            {
+                "data": "admin_fee"
+            },
+            {
+                "data": "status_produk"
+            },
+            {
+                "data": "aksi"
+            },
+        ],
+        "columnDefs": [{
+            "className": 'text-center',
+            "targets": [0, 1, 3, 4, 5, 6, 7]
+        }, ]
+    });
+
+
+    function tolak(uuid) {
+        var url = '{{ route("approval.produk.approve") }}';
+        $('#aproval-form').attr('action', url);
+        $('#id_produk').val(uuid)
+        swal({
+                $('#aproval-form').submit();
+                // title: "Apakah Anda Yakin !",
+                // text: "Ingin Menolak Data Ini ?.",
+                // type: "warning",
+                // showCancelButton: true,
+                // confirmButtonColor: "#802d34",
+                // confirmButtonText: "Ya, Tolak",
+                // cancelButtonText: "Cancel",
+                // closeOnConfirm: false,
+                // closeOnCancel: true
+            },
+            function(isConfirm) {
                 if (isConfirm) {
-                    $('#aproval-form').submit();
+                    $('#modal-approve').modal('show');
+
                 }
             });
-        }
-        function approve(id){
-            $('#id-produk').val(id);
-            $('#modal-approve').modal('show')
-        }
-        $(document).ready(function(){
-            $('#post-approve').parsley()
-        })
-    </script>
+    }
+
+    function approve(id) {
+        $('#id-produk').val(id);
+    }
+    $(document).ready(function() {
+        $('#post-approve').parsley()
+    })
+</script>
 @endsection
 
 @section('modal')
@@ -171,7 +190,7 @@
                     <input type="hidden" name="id_produk" id="id-produk">
                     <div class="form-group mb-4">
                         <h6 class="card-title">Alasan Di Approve</h6>
-                        <textarea class="form-control" name="keterangan" id="keterangan" placeholder="Masukan Keterangan" required></textarea>
+                        <textarea class="form-control" name="keterangan" id="keterangan" placeholder="Masukan Keterangan"></textarea>
                     </div>
 
                 </div>
